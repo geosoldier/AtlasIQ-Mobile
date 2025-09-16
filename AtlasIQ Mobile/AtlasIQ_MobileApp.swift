826004fc348f871ab98d74f2e7fdea2dae82c276
geosoldier <89037538+geosoldier@users.noreply.github.com>
2025-09-16 22:52:24 +0100

    Initial Commit

---
+++ AtlasIQ Mobile/AtlasIQ_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasIQ_MobileApp.swift
+//  AtlasIQ Mobile
+//
+//  Created by Eric Adams on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlasIQ_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtlasIQ MobileTests/AtlasIQ_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasIQ_MobileTests.swift
+//  AtlasIQ MobileTests
+//
+//  Created by Eric Adams on 9/16/25.
+//
+
+import Testing
+@testable import AtlasIQ_Mobile
+
+struct AtlasIQ_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
